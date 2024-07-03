cc60bd5b21121f4b20916265d3a7d08e7c27fd55
Lucas Dolsan <lucas.dolsan1775@gmail.com>
2024-07-03 18:19:58 -0300

    update pre-build script

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -1,3 +1,16 @@
-docker-compose build
+docker login
 
-docker-compose push
+echo "Building images..."
+
+docker build -t lucasdolsan/auth:latest ./auth
+docker build -t lucasdolsan/ingestion-api:latest ./ingestion-api
+docker build -t lucasdolsan/ingestion-worker:latest ./ingestion-worker
+docker build -t lucasdolsan/ingestion-ui:latest ./ingestion-ui
+docker build -t lucasdolsan/report-and-alerts:latest ./report-and-alerts
+
+echo "Pushing images..."
+docker push lucasdolsan/auth:latest
+docker push lucasdolsan/ingestion-api:latest
+docker push lucasdolsan/ingestion-worker:latest
+docker push lucasdolsan/ingestion-ui:latest
+docker push lucasdolsan/report-and-alerts:latest
